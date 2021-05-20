3ff025a0afe5c2a6e6dbe96c12c3b28bb4292383
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-05-20 02:10:19 +0100

    V1.0.0

---
+++ frontend/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import data from './data.js';
+
+function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
+  return (
+    <div className="grid-container">
+      <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <a href="index.html">amazona</a>
+            </div>
+            <div className="header-links">
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+            </div>
+        </header>
+        <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+            <ul>
+                <li>
+                    <a href="index.html">pants</a>
+                </li>
+                <li>
+                    <a href="index.html">Shirts</a>
+                </li>
+            </ul>
+        </aside>
+        <main className="main">
+            <div className="content">
+                <ul className="products">
+                  {data.products.map((product) => 
+                  <li>
+                        <div className="product">
+                            <img src={product.image} className="product-image" alt="product" />
+                            <div className="product-name">
+                                <a href="product.html">{product.name}</a>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">{product.price}</div>
+                            <div className="product-rating">{product.rating} Stars {product.numReviews}</div>
+                        </div>
+                    </li>
+                  )}
+                    
+                </ul>
+            </div>
+        </main>
+        <footer className="footer">
+            All right reserved
+        </footer>
+    </div>
+  );
+}
+
+export default App;
