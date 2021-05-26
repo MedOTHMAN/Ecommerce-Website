02a9cba3efc5b76b89bfaa79d4bf54a01a2f3aff
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-05-26 17:53:40 +0100

    V1.0.3

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ const app = express();
 app.get('/api/products', (req,res) =>{
     res.send(data.products);
 })
-
-const hello = (req,res,next) => {
-    console.log("hello from middleware");
-};
-app.use(hello);
+app.get('/api/products/:id', (req,res) =>{
+    const productId = req.params.id;
+    const product = data.products.find(x => x._id === productId);
+    if(product)
+        res.send(product);
+    else
+    res.status(404).send({msg: "Product Not Found"})
+});
 
 app.listen(5000,(err) =>{
     if(err) throw(err)
